41ad70531d10783f355d56772a0d3fd98324470a
Paweł Cichowski <pcichowski@pcichowski.com>
2024-04-09 08:23:59 +0200

    RSWW-27 add material ui
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import './App.css';
 // @ts-ignore
 import logo from './logo.svg';
+import {Button} from "@mui/material";
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header gap-4">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p className='text-xl'>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <Button size='medium' variant='contained' className='mb-12'>Material UI test</Button>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {ThemeProvider} from "@mui/material";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
