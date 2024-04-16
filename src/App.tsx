cfc7d3d61890a5f907c9169687cb2d36ce45606a
Paweł Cichowski <pcichowski@pcichowski.com>
2024-04-16 12:03:38 +0200

    RSWW-28 Navbar: add component
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Offers from "./offers/screens/Offers";
 import NotFound from "./core/screens/NotFound";
+import Navbar from './core/components/Navbar';
 
 function App() {
     return (
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path='/' element={<Offers/>}/>
                 <Route path="*" element={<NotFound />} />
---
+++ src/core/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import {Button} from "@mui/material";
+// @ts-ignore
+import logo from '../../assets/tourcentral.png'
+import LoginIcon from '@mui/icons-material/Login';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+export default function Navbar () {
+    return (
+        <div className='flex flex-row items-center justify-between mx-6 px-6 py-2 border-b-gray-100' style={{borderBottomWidth: 1.2}}>
+            <div>
+                <img src={logo} style={{maxHeight: '60px', pointerEvents: 'none'}} alt="logo"/>
+            </div>
+            <ul className='flex flex-row gap-12'>
+                <li className='flex flex-row items-center'>
+                    <Button
+                        variant='text'
+                        endIcon={<ArrowDropDownIcon style={{color: '#333'}}/>}
+                        style={{color: '#333'}}
+                    >
+                        Offers
+                    </Button>
+                </li>
+                <li className='flex flex-row items-center'>
+                    <Button
+                        variant='text'
+                        endIcon={<ArrowDropDownIcon style={{color: '#333'}}/>}
+                        style={{color: '#333'}}
+                    >
+                        Destinations
+                    </Button>
+                </li>
+
+            </ul>
+            <div>
+                <Button variant='contained' startIcon={<LoginIcon/>}>Log in</Button>
+            </div>
+        </div>
+    );
+}
