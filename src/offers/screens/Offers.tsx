757bb1dcf146e2f8cfb08355e75c09976fc1576e
Paweł Cichowski <pcichowski@pcichowski.com>
2024-05-18 19:53:37 +0200

    RSWW-100 Offers: add no results message
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- src/offers/screens/Offers.tsx
+++ src/offers/screens/Offers.tsx
@@ -1,6 +1,7 @@
 import OfferComponent from "../components/OfferComponent";
 import {useEffect, useState} from "react";
 import {ApiRequests, GetOffersBySearchQueryOffer} from "../../core/apiConfig";
+import {SentimentVeryDissatisfied} from "@mui/icons-material";
 
 export default function Offers () {
 
@@ -22,9 +23,11 @@ export default function Offers () {
 
     return(
         <div className='flex flex-col py-16 px-[28rem] justify-center'>
-            <div className='offersHeaderContainer'>
-                <h1 className='text-2xl mb-12'>Excellent offers just for you!</h1>
-            </div>
+            {offers.length > 0 &&
+                <div className='offersHeaderContainer'>
+                    <h1 className='text-2xl mb-12'>Excellent offers just for you!</h1>
+                </div>
+            }
 
             {offers.map((offer, index) => (
                 <OfferComponent
@@ -38,28 +41,15 @@ export default function Offers () {
                 />
             ))}
 
-            <OfferComponent
-                name='Ibiza Gwiździny'
-                location='Polska, Nowe Miasto Lubawskie'
-                rating={4.1}
-                pricePerPerson={1399}
-            />
-
-            <OfferComponent
-                name='Hotel Brzeźno'
-                location='Polska, Gdańsk'
-                rating={4.4}
-                pricePerPerson={799}
-                photoURL='https://picsum.photos/id/853/400/200'
-            />
-
-            <OfferComponent
-                name='Odysee Resort'
-                location='Tunezja, Tunis'
-                rating={5.3}
-                pricePerPerson={2199}
-                photoURL='https://picsum.photos/id/386/400/200'
-            />
+            {offers.length === 0 &&
+                <div className='flex flex-col gap-4 mt-12'>
+                    <div className='flex flex-row items-center gap-2'>
+                        <SentimentVeryDissatisfied style={{fontSize: 36}}/>
+                        <p className='text-2xl'>No results</p>
+                    </div>
+                    <p>Change your search parameters and try again...</p>
+                </div>
+            }
         </div>
     );
 }
