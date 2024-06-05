5b3b8e6aa8b7cb4678a0038993cca84e331a7001
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-06-05 20:52:09 +0200

    RSWW-138 set up connection with backend and receive data

--- src/tour-operator-updates/screens/TOUpdates.tsx
+++ src/tour-operator-updates/screens/TOUpdates.tsx
@@ -1,6 +1,15 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {ConnectingAirports, Hotel} from "@mui/icons-material";
-import React from "react";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { ConnectingAirports, Hotel } from "@mui/icons-material";
+import React, { useEffect, useState } from "react";
+
+type HotelUpdate = {
+    updateDateTime: string;
+    updateType: string;
+    hotelName: string;
+    roomName: string;
+    priceChange: number;
+    capacityChange: number;
+};
 
 const TOUpdates = () => {
 
@@ -14,30 +23,47 @@ const TOUpdates = () => {
                 capacityDiff: -20,
                 priceDiff: -48,
             }
-        },
-        {
-            date: new Date(),
-            locationFrom: {idLocation: '124', region: 'Gdańsk', country: 'Polska'},
-            locationTo: {idLocation: '345', region: 'Durres', country: 'Albania'},
-            type: 'PLANE',
-            changes: {
-                capacityDiff: 34,
-                priceDiff: 125,
-            }
-        },
+        }
     ]
 
-    const hotelChanges = [
-        {
-            date: new Date(),
-            hotelName: 'Grand Fafa Blue',
-            roomName: 'Pokój 2 os.',
-            changes: {
-                guestCapacityDiff: 1,
-                priceDiff: 27,
+    const [hotelUpdates, setHotelUpdates] = useState<HotelUpdate[]>([]);
+
+    useEffect(() => {
+        const ws = new WebSocket(`ws://localhost:8086/data-generator/ws/hotel`);
+
+        ws.onmessage = (event) => {
+            console.log("Received message: " + event.data);
+        
+            const messageType = event.data.split(':')[0];
+            const messageData = event.data.split(': ')[1];
+        
+            switch (messageType) {
+                case "SingleHotel":
+                    const hotelUpdate = JSON.parse(messageData);
+        
+                    // Convert the updateDateTime array to a Date object
+                    hotelUpdate.updateDateTime = new Date(
+                        hotelUpdate.updateDateTime[0],
+                        hotelUpdate.updateDateTime[1] - 1, // JavaScript months are 0-based
+                        hotelUpdate.updateDateTime[2],
+                        hotelUpdate.updateDateTime[3],
+                        hotelUpdate.updateDateTime[4]
+                    );
+        
+                    setHotelUpdates((prevUpdates) => [hotelUpdate, ...prevUpdates]);
+                    break;
+                default:
+                    console.log("Unexpected message type");
+                    break;
             }
-        },
-    ]
+        };        
+
+        return () => {
+            ws.close();
+            console.error("WebSocket connection closed");
+        };
+    }, []);
+
 
     return (
         <div className='flex flex-col px-16 py-24'>
@@ -79,9 +105,9 @@ const TOUpdates = () => {
                     </TableContainer>
                 </Paper>
 
-                <Paper elevation={2} style={{height: 'fit-content'}}>
+                <Paper elevation={2} style={{ height: 'fit-content' }}>
                     <div className='mt-4 mb-6 flex flex-row gap-2 items-center justify-center'>
-                        <Hotel style={{fontSize: 18}}/>
+                        <Hotel style={{ fontSize: 18 }} />
                         <h3 className='text-xl'>Aktualizacje hoteli</h3>
                     </div>
 
@@ -96,15 +122,15 @@ const TOUpdates = () => {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {hotelChanges.map((hotel, index) => (
+                                {hotelUpdates.map((hotel, index) => (
                                     <TableRow key={index}>
-                                        <TableCell className='border border-gray-300 p-2'>{hotel.date.toDateString()}</TableCell>
+                                        <TableCell className='border border-gray-300 p-2'>{new Date(hotel.updateDateTime).toLocaleString()}</TableCell>
                                         <TableCell className='border border-gray-300 p-2'>{hotel.hotelName}</TableCell>
                                         <TableCell className='border border-gray-300 p-2'>{hotel.roomName}</TableCell>
                                         <TableCell className='border border-gray-300 p-2'>
                                             <div className='flex flex-col gap-1'>
-                                                <p>Cena: {hotel.changes.priceDiff >= 0 ? '+' : ''}{hotel.changes.priceDiff}</p>
-                                                <p>Miejsca: {hotel.changes.guestCapacityDiff >= 0 ? '+' : ''}{hotel.changes.guestCapacityDiff}</p>
+                                                <p>Cena: {hotel.priceChange >= 0 ? '+' : ''}{hotel.priceChange}</p>
+                                                <p>Miejsca: {hotel.capacityChange >= 0 ? '+' : ''}{hotel.capacityChange}</p>
                                             </div>
                                         </TableCell>
                                     </TableRow>
